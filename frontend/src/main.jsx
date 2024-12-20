92dae0fdd737a51bbc7e9b931695c5a52609a18e
hydoor-ahmed <haidar.craft.96@gmail.com>
2024-12-20 12:35:14 +0300

    .

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,14 +4,14 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { AuthContextProvider } from "./context/AuthContext.jsx";
-import { SocketContextProvider } from "./Context/SocketContext.jsx";
+import { SocketContextProvider } from "./context/SocketContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <AuthContextProvider>
         <SocketContextProvider>
-        <App />
+          <App />
         </SocketContextProvider>
       </AuthContextProvider>
     </BrowserRouter>
